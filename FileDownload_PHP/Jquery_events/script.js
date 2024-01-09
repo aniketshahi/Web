6a14e0130af04a8c2beabb93885426e470a6c8d9
Aniket Shahi <109075043+aniket0608@users.noreply.github.com>
2024-01-09 22:01:33 +0530

    update

--- FileDownload_PHP/Jquery_events/script.js
+++ FileDownload_PHP/Jquery_events/script.js
@@ -1,10 +1,13 @@
-$(document).ready(function(){
-    $("#animateButton").click(function(){
-        $("#animateBox").animate({
-            left: '250px',
-            opacity: '0.5',
-            height: '150px',
-            width: '150px'
-        }, 1500);
-    });
+$(document).ready(function () {
+  $("#animateButton").click(function () {
+    $("#animateBox").animate(
+      {
+        left: "250px",
+        opacity: "0.5",
+        height: "150px",
+        width: "150px",
+      },
+      1500
+    );
+  });
 });
